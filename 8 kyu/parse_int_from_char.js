a8c546fc013464659efeef353a3700ef01f7194d
Nat Geyzen <natgeyzen.tech@aol.com>
2022-06-01 13:03:19 -0500

    Returns a parsed integer from a string

---
+++ 8 kyu/parse_int_from_char.js
@@ -0,0 +1,34 @@
+/* INSTRUCTIONS:
+   You ask a small girl,"How old are you?" She always says, "x years old", where x
+   is a random number between 0 and 9.
+
+   Write a program that returns the girl's age (0-9) as an integer.
+
+   Assume the test input string is always a valid string. For example, 
+   the test input may be "1 year old" or "5 years old". 
+   The first character in the string is always a number.
+*/ 
+
+// SOLUTION 1A: Using parseIst()
+function getAge(inputString){
+    return parseInt(inputString);
+}
+
+// SOLUTION 1B: ES6 arrow function alternative
+const getAge = inputString => parseInt(inputString);
+
+// SOLUTION 2A: Using Number() and charAt()
+function getAge(inputString){
+    return Number(inputString[0]);
+}
+
+// SOLUTION 2B: ES6 arrow function alternative
+const getAge = inputString => Number(inputString[0]);
+
+// SOLUTION 3A: Using the unary operator and regex
+function getAge(inputString){
+    return +inputString.match(/\d/);
+}
+
+// SOLUTION 3B: ES6 arrow function alternative
+const getAge = inputString => +inputString.match(/\d/);

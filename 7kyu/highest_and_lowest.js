00f3170e322b7a37611c71b027cd1f52349658f4
Nat Geyzen <natgeyzen.tech@aol.com>
2022-06-14 13:01:29 -0500

    Returns the highest and lowest numbers from a string of numbers

--- 7kyu/highest_and_lowest.js
+++ 7kyu/highest_and_lowest.js
@@ -13,22 +13,50 @@
    - Output string must be two numbers separated by a single space, and highest number is first.
 */ 
 
-// SOLUTION 1A: 
+// SOLUTION 1A: Looping over the numbers
+function highAndLow(numbers){
+  const arr = numbers.split(' ').map(Number);
+  let highest = arr[0];
+  let lowest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > highest) highest = arr[i];
+    if (arr[i] < lowest) lowest = arr[i];
+  }
+  return highest + ' ' + lowest;
+}
+
+// SOLUTION 1B: ES6 arrow function alternative
+const highAndLow = numbers => {
+  const arr = numbers.split(' ').map(Number);
+  let highest = arr[0];
+  let lowest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > highest) highest = arr[i];
+    if (arr[i] < lowest) lowest = arr[i];
+  }
+  return highest + ' ' + lowest;
+}
+
+// SOLUTION 2A: Sorting the numbers
+function highAndLow(numbers){
+  const sortedNums = numbers.split(' ').sort((a, b) => a - b);
+  return [sortedNums[sortedNums.length - 1], sortedNums[0]].join(' ');
+}
+
+// SOLUTION 2B: ES6 arrow function alternative
+const highAndLow = numbers => {
+  const sortedNums = numbers.split(' ').sort((a, b) => a - b);
+  return [sortedNums[sortedNums.length - 1], sortedNums[0]].join(' ');
+}
+
+// SOLUTION 3A: Extracting the max and min number
 function highAndLow(numbers){
   const numArr = numbers.split(' ');
   return `${Math.max(...numArr)} ${Math.min(...numArr)}`;
 }
 
-// SOLUTION 1B: ES6 arrow function alternative
+// SOLUTION 3B: ES6 arrow function alternative
 const highAndLow = numbers => {
   const numArr = numbers.split(' ');
   return `${Math.max(...numArr)} ${Math.min(...numArr)}`;
 }
-
-// SOLUTION 2A: 
-
-// SOLUTION 2B: ES6 arrow function alternative
-
-// SOLUTION 3A: 
-
-// SOLUTION 3B: ES6 arrow function alternative
